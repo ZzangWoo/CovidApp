7ebfd1e0d027c27e9df0e3485f57638f679672ce
ZzangWoo <whdlwns20@gmail.com>
2020-12-30 20:41:34 +0900

    레이아웃 초기화

--- app/src/main/java/com/example/covidapp/MainActivity.kt
+++ app/src/main/java/com/example/covidapp/MainActivity.kt
@@ -7,21 +7,8 @@ import androidx.viewpager.widget.PagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    var view_list = ArrayList<View>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    inner class CustomAdapter : PagerAdapter() {
-        override fun isViewFromObject(view: View, `object`: Any): Boolean {
-            return view == `object`
-        }
-
-        override fun getCount(): Int {
-            return view_list.size
-        }
-
     }
 }
